491554707dace40cccbb18f4694e8f2926c10984
SatineToo <1045985201@qq.com>
2015-04-28 13:46:03 +0800

    hehe
    1501

---
+++ myBox/视图/基础视图/BaseTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  BaseTabBarController.h
+//  myBox
+//
+//  Created by cyt on 15/4/28.
+//  Copyright (c) 2015年 cyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseTabBarController : UITabBarController
+
+@end
